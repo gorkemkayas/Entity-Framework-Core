1b7dd33d5ef6d52eff76412ec53648dd2289bcd0
= <gorkemkayas@hotmail.com>
2024-08-19 18:53:01 +0300

    EFCore course sessions

---
+++ EFCore/EFCore.DatabaseFirstByScaffold/Program.cs
@@ -0,0 +1,9 @@
+﻿using EFCore.DatabaseFirstByScaffold.Models;
+using Microsoft.EntityFrameworkCore;
+
+using ( var context = new EfcoreDatabaseFirstDbContext())
+{
+    var products = await context.Products.ToListAsync();
+
+    products.ForEach(p => Console.WriteLine($"{p.Id} : {p.Name} - {p.Price}"));
+}
